4b1269a0f0f21677689cb68ecf19bf9f107ed976
Evan <g00348451@gmit.ie>
2021-02-17 13:47:28 +0000

    Bit Operations first attempt completed

--- bitOperations.c
+++ bitOperations.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
+void bin_print(int i){
+    // Number of bits in an Integer
+    int j = (sizeof(int)*8)-1;
+    //Temporary variable
+    int k;
+
+    for (j--; j >= 0; j--)
+    {
+        k= ((1 << j) & i) ? 1 : 0;
+        printf("%d",k);
+
+    }
+     
+
+}
+
 int main(int argc, char *argv[]){
-    //print hello world
-    printf("%s\n", "Hello World! 2.0");
+   
+    int i = 241;
+    printf("Original: ");
+    bin_print(i);
+    printf("\n");
+
+    for ( int j = 0; j < 40; j++)
+    {
+        printf("%3d << %2d: ",i,j);
+        bin_print(i << j);
+        printf("\n");
+    }
+    
 
     
     return 0;
