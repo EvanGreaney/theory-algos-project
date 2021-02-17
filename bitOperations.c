b8c7af66351a9885ad8d0d8f963533cfad4035bc
Evan <g00348451@gmit.ie>
2021-02-17 14:10:22 +0000

    bit perations with a 32bit full integer

--- bitOperations.c
+++ bitOperations.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-void bin_print(int i){
+void bin_print(unsigned int i){
     // Number of bits in an Integer
-    int j = (sizeof(int)*8)-1;
+    int j = (sizeof(unsigned int)*8)-1;
     //Temporary variable
     int k;
-
+    // Loop over the number of bits in i.
     for (j--; j >= 0; j--)
     {
         k= ((1 << j) & i) ? 1 : 0;
+        //print k;
         printf("%d",k);
 
     }
@@ -17,16 +18,22 @@ void bin_print(int i){
 }
 
 int main(int argc, char *argv[]){
-   
-    int i = 241;
-    printf("Original: ");
+    //Set i to literal value
+    unsigned int i = 4294967295;
+    //What we're Printing
+    printf("Original:  ");
+    //Print i in binary
     bin_print(i);
-    printf("\n");
+    // End Line
+    printf("\t%x\t%u\n",i , i);
 
     for ( int j = 0; j < 40; j++)
     {
-        printf("%3d << %2d: ",i,j);
+        //what the operation is
+        printf("%3u << %2d: ",i,j);
+        //i shifted left j times
         bin_print(i << j);
+        //End Line
         printf("\n");
     }
     
