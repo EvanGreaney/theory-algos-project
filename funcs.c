fb6da5e0a1a68931484dbd941318fa6b0d142bca
Evan <g00348451@gmit.ie>
2021-03-02 12:18:55 +0000

    Added the Sigmatic SHA-256 Functions to the func.c along with the SHR function

--- funcs.c
+++ funcs.c
@@ -5,15 +5,19 @@
 #define WORD uint32_t
 #define PF PRIX32
 
-// Page 10 of the Secure Hash Standard.
-#define CH(x,y,z) (x&y)^(~x&z)
-#define MAJ(x,y,z) (x&y)^(x&z)^(y&z)
-
-
 // Page 5 of the Secure Hash Standard.
 #define ROTL(x,n) (x<<n)|(x>>(W-n))
 #define ROTR(x,n) (x>>n)|(x<<(W-n))
+#define SHR(x,n) x>>n
 
+
+// Page 10 of the Secure Hash Standard.
+#define CH(x,y,z) (x&y)^(~x&z)
+#define MAJ(x,y,z) (x&y)^(x&z)^(y&z)
+#define SIG0(x) ROTR(x,2)^ROTR(x,13)^ROTR(x,22)
+#define SIG1(x) ROTR(x,6)^ROTR(x,11)^ROTR(x,25)
+#define Sig0(x) ROTR(x,7)^ROTR(x,18)^SHR(x,3)
+#define Sig1(x) ROTR(x,17)^ROTR(x,19)^SHR(x,10)
 
 
 
@@ -26,7 +30,7 @@ WORD Ch(WORD x, WORD y , WORD z){
 int main(int argc, char *argv[]){
     
     
-    WORD x = 0x1234567;
+    WORD x = 0xF1234567;
     WORD y = 0x0A0A0A0A;
     WORD z = 0xB0B0B0B0;
 
@@ -44,6 +48,11 @@ int main(int argc, char *argv[]){
 
     printf("ROTL(%08"PF" -> %08"PF "\n",x ,ROTL(x,4));
     printf("ROTR(%08"PF" -> %08"PF "\n",x ,ROTR(x,4));
+    printf("SHR(%08"PF" -> %08"PF "\n",x ,SHR(x,4));
+    printf("SIG0(%08"PF" -> %08"PF "\n",x ,SIG0(x));
+    printf("SIG1(%08"PF" -> %08"PF "\n",x ,SIG1(x));
+    printf("Sig0(%08"PF" -> %08"PF "\n",x ,Sig0(x));
+    printf("Sig1(%08"PF" -> %08"PF "\n",x ,Sig1(x));
     
     
     return 0;
