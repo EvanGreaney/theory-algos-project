af25a1d92ca0f426d56487ee2d4e4f8643307a2c
Evan <g00348451@gmit.ie>
2021-03-08 18:02:16 +0000

    prepoc.c is now reading inout from a file

---
+++ prepoc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#define BYTE uint8_t
+
+int main(int argc, char *argv[])
+{
+    BYTE b;
+
+    FILE *f;
+
+    f= fopen(argv[1], "r");
+
+    size_t nobytes;
+
+    nobytes = fread(&b,1,1,f);
+    printf("%c", b);
+    while (nobytes)
+    {
+        nobytes = fread(&b,1,1,f);
+        printf("%c", b);
+    }
+    fclose(f);
+    printf("\n");
+    
+    return 0;
+}
